6e3a86f3065b247d986376c3d2dd0daafcecf7e6
Brianna Forster <brianna.forster@gmail.com>
2017-09-05 17:23:20 -0700

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 end
