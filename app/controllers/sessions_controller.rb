bb0ff6dc7541f378b5ce6e45bdfa88a2e521f2dc
Brianna Forster <brianna.forster@gmail.com>
2017-09-16 19:10:06 -0700

    Create login form and controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log in, redirect to user show page
+    else
+      # Create an error message
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
