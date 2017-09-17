3b98ae2c920b083d60d93915624814a2a0d62bee
Brianna Forster <brianna.forster@gmail.com>
2017-09-16 19:15:07 -0700

    Create flash message and pass test for invalid user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log in, redirect to user show page
     else
-      # Create an error message
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
